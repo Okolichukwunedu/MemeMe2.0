8759052d934dd84996f85cdf2f993abc25f0d97c
Okoli Chinedu Christian <Okolichukwunedu@gmail.com>
2022-08-18 19:00:49 +0100

    Add files via upload

---
+++ memeMe2.0 Project/memeMe1.0 Project/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  MemeMeStruct.swift
+//  EditMeMe1.0
+//
+//  Created by Okoli-Chinedu on 19/07/2022.
+//  Copyright © 2022 Okoli-Chinedu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct Meme{
+    var topText = String()
+    var bottomText = String()
+    var originalImage = UIImage()
+    var memedImage = UIImage()
+}
